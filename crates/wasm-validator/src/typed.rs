35b271af5e4e9d03f7dbf5d947658c21a0b596bd
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-20 23:36:36 +0800

    feat(simd): validate i8x16 wrapping arithmetic

--- crates/wasm-validator/src/typed.rs
+++ crates/wasm-validator/src/typed.rs
@@ -1042,7 +1042,21 @@ pub(super) fn validate_code(
                         pop_expect(&mut stack, &controls, ValueType::V128, function, offset)?;
                         stack.push(ValueType::V128);
                     }
-                    101 | 102 | 111 | 112 | 114 | 115 | 118 | 119 | 120 | 121 | 123 | 133 | 134 => {
+                    101
+                    | 102
+                    | 110
+                    | 111
+                    | 112
+                    | 113
+                    | 114
+                    | 115
+                    | 118
+                    | 119
+                    | 120
+                    | 121
+                    | 123
+                    | 133
+                    | 134 => {
                         pop_expect(&mut stack, &controls, ValueType::V128, function, offset)?;
                         pop_expect(&mut stack, &controls, ValueType::V128, function, offset)?;
                         stack.push(ValueType::V128);
